ffe1dd27e2dc214ebd6d3bb55e3f7911d8bb7077
Dimitar Kumanov <dimitark@uw.edu>
2018-12-06 16:56:59 -0800

    Updates queries.sql capitalization & formatting.

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -10,8 +10,8 @@
 * Used In: LogIn.aspx during loging in to determine whether the employee exists.
 */
 Select *
-From EMPLOYEE
-Where EMPLOYEE.ID = {0};
+From Employee
+Where Employee.Id = {0};
 
 /* QUERY 2
 * Objective: Look up employee position based on the employee id.
@@ -39,11 +39,11 @@ Where Employee.Id = {0};
     {0} - employee id
 * Used In: LogIn.aspx to determine whether the employee has checked out a tablet already. This is used to decide if the employee should be redirected to UserView_CheckOut.aspx or UserView_Return.aspx.
 */ 
-Select DEVICE_HANDLE.DeviceId, DEVICE_RECORD.CheckOutTime
-From DEVICE_HANDLE, DEVICE_RECORD
-Where {0} = DEVICE_HANDLE.EmployeeId AND
-    DEVICE_HANDLE.DeviceRecordId = DEVICE_RECORD.ID AND
-    DEVICE_RECORD.ReturnTime IS NULL;
+Select Device_Handle.DeviceId, Device_Record.CheckOutTime
+From Device_Handle, Device_Record
+Where {0} = Device_Handle.EmployeeId AND
+    Device_Handle.DeviceRecordId = Device_Record.Id AND
+    Device_Record.ReturnTime IS NULL;
 
 
 /* QUERY 5 (multiple queries, but treated as a single transaction)
@@ -55,12 +55,12 @@ Where {0} = DEVICE_HANDLE.EmployeeId AND
     {3} - the ID of the device being checked out
 * Used In: UserView_CheckOut.aspx to check out the chosen device by the logged in user.
 */ 
-{0} = (Select MAX(ID) From DEVICE_RECORD; + 1)
+{0} = (Select MAX(ID) From Device_Record; + 1)
 
-Insert Into DEVICE_RECORD(ID, CheckOutTime, ReturnTime)
-Values({0}newDeviceRecordId, {1} dateTimeStr, NULL);
-Insert Into DEVICE_HANDLE
-Values ({2} logedInemployeeId, {3} deviceId, {0} newDeviceRecordId;
+Insert Into Device_Record(ID, CheckOutTime, ReturnTime)
+Values({0}, {1}, NULL);
+Insert Into Device_Handle
+Values ({2}, {3}, {0});
 Update Device
 Set IsCheckedOut = 1
 Where Device.ID = {3};
@@ -72,13 +72,13 @@ Where Device.ID = {3};
     {0} - employee id
 * Used In: UserView_Return.aspx to show the device ID, make and model on the page so the user knows which device they would be returning.
 */
-Select Coalesce(STR(DEVICE.ID), Device_MODEL.Make, DEVICE_MODEL.Model, '') as DeviceIdMakeModel
-From DEVICE_HANDLE, DEVICE_RECORD, DEVICE, DEVICE_MODEL
-Where {0} = DEVICE_HANDLE.EmployeeId AND
-    DEVICE_HANDLE.DeviceRecordId = DEVICE_RECORD.ID AND
-    DEVICE_RECORD.ReturnTime IS NULL AND
-    DEVICE.ID = DEVICE_HANDLE.DeviceId AND
-    DEVICE_MODEL.PurchaseDate = DEVICE.PurchaseDate;
+Select Coalesce(STR(Device.Id), Device_Model.Make, Device_Model.Model, '') as DeviceIdMakeModel
+From Device_Handle, Device_Record, DEVICE, Device_Model
+Where {0} = Device_Handle.EmployeeId AND
+    Device_Handle.DeviceRecordId = Device_Record.Id AND
+    Device_Record.ReturnTime IS NULL AND
+    Device.Id = Device_Handle.DeviceId AND
+    Device_Model.PurchaseDate = Device.PurchaseDate;
 
 /* QUERY 7
 * Objective: Determine the id of the device checked out by an employee.
@@ -86,13 +86,13 @@ Where {0} = DEVICE_HANDLE.EmployeeId AND
     {0} - employee id
 * Used In: UserView_Return.aspx to find the id of the device that the user is returning which is used to update the necessery relations. (see QUERY 9)
 */
-Select DEVICE.ID
-From DEVICE_HANDLE, DEVICE_RECORD, DEVICE, DEVICE_MODEL
-Where {0} = DEVICE_HANDLE.EmployeeId AND
-    DEVICE_HANDLE.DeviceRecordId = DEVICE_RECORD.ID AND
-    DEVICE_RECORD.ReturnTime IS NULL AND
-    DEVICE.ID = DEVICE_HANDLE.DeviceId AND
-    DEVICE_MODEL.PurchaseDate = DEVICE.PurchaseDate;
+Select Device.Id
+From Device_Handle, Device_Record, DEVICE, Device_Model
+Where {0} = Device_Handle.EmployeeId AND
+    Device_Handle.DeviceRecordId = Device_Record.Id AND
+    Device_Record.ReturnTime IS NULL AND
+    Device.Id = Device_Handle.DeviceId AND
+    Device_Model.PurchaseDate = Device.PurchaseDate;
 
 /* QUERY 8
 * Objective: Determine the device record id for the device record containing information for the device currently checked out by an employee.
@@ -100,13 +100,13 @@ Where {0} = DEVICE_HANDLE.EmployeeId AND
     {0} - employee id
 * Used In: UserView_Return.aspx to find out the device record id, since that record needs to be updated. This is needed since the return time in the device record needs to be set to the current time date. (see QUERY 9)
 */
-Select DEVICE_RECORD.ID
-From DEVICE_HANDLE, DEVICE_RECORD, DEVICE, DEVICE_MODEL
-Where {0} = DEVICE_HANDLE.EmployeeId AND
-    DEVICE_HANDLE.DeviceRecordId = DEVICE_RECORD.ID AND
-    DEVICE_RECORD.ReturnTime IS NULL AND
-    DEVICE.ID = DEVICE_HANDLE.DeviceId AND
-    DEVICE_MODEL.PurchaseDate = DEVICE.PurchaseDate;
+Select Device_Record.Id
+From Device_Handle, Device_Record, DEVICE, Device_Model
+Where {0} = Device_Handle.EmployeeId AND
+    Device_Handle.DeviceRecordId = Device_Record.Id AND
+    Device_Record.ReturnTime IS NULL AND
+    Device.Id = Device_Handle.DeviceId AND
+    Device_Model.PurchaseDate = Device.PurchaseDate;
 
 
 /* QUERY 9 (multiple queries, but treated as a single transaction)
@@ -119,32 +119,32 @@ Where {0} = DEVICE_HANDLE.EmployeeId AND
 */ 
 Update Device
 Set IsCheckedOut = 0
-Where Device.ID = {0};
-Update DEVICE_RECORD
+Where Device.Id = {0};
+Update Device_Record
 Set ReturnTime = {2}
-Where DEVICE_RECORD.ID = {1};
+Where Device_Record.Id = {1};
 
 /* QUERY 10
 * Objective: Show all device records, including the ID of the device being checked out, the name, id and phone of the employee checking out the device, the time the device was checked out and returned (or empty if still checked out). Additionally, results are sorted by check out time in order to show the most recent device usages first.
 * Arguments: None
 * Used In: ManagerView.aspx to display all device records for the manager to inspect and potentially contact employees that have not returned their devices.
 */ 
-Select DEVICE_HANDLE.DeviceId, EMPLOYEE.Fname, EMPLOYEE.Lname,
-    Employee.Phone as PhoneNumber, EMPLOYEE.ID as EmployeeID,
-    DEVICE_RECORD.CheckOutTime, DEVICE_RECORD.ReturnTime
-From EMPLOYEE, DEVICE_HANDLE, DEVICE_RECORD
-Where DEVICE_RECORD.ID = DEVICE_HANDLE.DeviceRecordId AND
-    EMPLOYEE.ID = DEVICE_HANDLE.EmployeeId
-Order By DEVICE_RECORD.CheckOutTime DESC;
+Select Device_Handle.DeviceId, EMPLOYEE.Fname, EMPLOYEE.Lname,
+    Employee.Phone as PhoneNumber, EMPLOYEE.Id as EmployeeID,
+    Device_Record.CheckOutTime, Device_Record.ReturnTime
+From EMPLOYEE, Device_Handle, Device_Record
+Where Device_Record.Id = Device_Handle.DeviceRecordId AND
+    EMPLOYEE.Id = Device_Handle.EmployeeId
+Order By Device_Record.CheckOutTime DESC;
 
 /* QUERY 11
 * Objective: Show the device id and also the device id, make and model as a single attribute for all devices that can be checked out.
 * Arguments: None
 * Used In: UserView_CheckOut.aspx to display a menu with all devices that are available for check out. The ID, make, model need to be shown as a single attribute due to a limitation of Visual Studio's menu to only be capable of displaying a single attribute from the query results.
 */ 
-Select DEVICE.ID,
-    Coalesce(STR(DEVICE.ID), Device_MODEL.Make, DEVICE_MODEL.Model, '') as DeviceIdMakeModel
-From DEVICE_MODEL, DEVICE
-Where DEVICE_MODEL.PurchaseDate = DEVICE.PurchaseDate 
-    AND device.IsCheckedOut = 0
-Order By DEVICE_MODEL.PurchaseDate;
+Select Device.Id,
+    Coalesce(STR(Device.Id), Device_Model.Make, Device_Model.Model, '') as DeviceIdMakeModel
+From Device_Model, DEVICE
+Where Device_Model.PurchaseDate = Device.PurchaseDate 
+    AND Device.IsCheckedOut = 0
+Order By Device_Model.PurchaseDate;
